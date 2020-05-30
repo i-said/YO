dbacf90b62d0e1a4ce72a3f4059acea5b4957a9b
ikasumiwt <kato@atama.plus>
2020-05-30 23:02:48 +0900

    add components

---
+++ components/TalkingButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <button class="card" @click="createAloRoom">
+      <div class="card-content">
+        <div class="content yo-button has-text-centered">
+
+        </div>
+        <slot />
+      </div>
+    </button>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const host = 'localhost:3001';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  props: {
+  },
+  methods: {
+    createAloRoom() {
+      // TODO なにかルームが作られる
+      alert("Aloしてくれるユーザーを待ちます");
+      if (!this.$parent.socket) {
+        this.$parent.socket = io(host);
+      }
+      this.$parent.socket.emit('waiting-talk')
+
+      // TODO: roomのほうにoverlayしてやるほうがよさそう
+      this.$parent.isLoading = true;
+    }
+
+  }
+}
+</script>
