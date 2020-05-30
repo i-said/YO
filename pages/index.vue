d062590db438009a5fe3fcfad7d0879109195be8
ikasumiwt <kato@atama.plus>
2020-05-30 15:49:23 +0900

    status updated

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="YO!"
+        icon="alert-decagram"
+      >
+      Say, YO!
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
