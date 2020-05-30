ab655527f97de9b850b5a8a742bbcae778ee2903
fuiwaseb <fuiwaseb@yahoo-corp.jp>
2020-05-30 23:52:15 +0900

    add webrtc logic powerd by skyway

--- pages/calling/index.vue
+++ pages/calling/index.vue
@@ -1,34 +1,141 @@
 <template>
   <section class="section">
-
     <div class="columns is-mobile is-centered">
-      きっとこのへんでskywayされるはず
+      <div class="container">
+        <h1 class="heading">P2P Media example</h1>
+        <p class="note">Enter remote peer ID to call.</p>
+        <div class="p2p-media">
+          <div class="remote-stream">
+            <video id="js-remote-stream" controls></video>
+          </div>
+          <div class="local-stream">
+            <video id="js-local-stream" controls></video>
+            <p>
+              Your ID:
+              <span id="js-local-id"></span>
+            </p>
+            <input type="text" placeholder="Remote Peer ID" id="js-remote-id" />
+            <!-- 自動的に接続するならCallボタンは不要 -->
+            <button id="js-call-trigger">Call</button>
+            <button id="js-close-trigger">Close</button>
+          </div>
+        </div>
+        <p class="meta" id="js-meta"></p>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import TalkingButton from '~/components/TalkingButton'
-import io from 'socket.io-client'
-const host = 'localhost:3001';
-
+import Peer from "skyway-js";
+import TalkingButton from "~/components/TalkingButton";
+import io from "socket.io-client";
+const host = "localhost:3001";
 
 export default {
-  name: 'index',
+  name: "index",
   data() {
     return {
       waitingUsers: [],
-      socket: '',
+      socket: "",
       isLoading: false
-    }
+    };
   },
 
   components: {
     TalkingButton
   },
   mounted() {
-    console.log("mounted path: /calling")
-  }
+    console.log("mounted path: /calling");
 
-}
+    // const Peer = window.Peer;
+
+    (async function main() {
+      const localVideo = document.getElementById("js-local-stream");
+      const localId = document.getElementById("js-local-id");
+      const callTrigger = document.getElementById("js-call-trigger");
+      const closeTrigger = document.getElementById("js-close-trigger");
+      const remoteVideo = document.getElementById("js-remote-stream");
+      const remoteId = document.getElementById("js-remote-id");
+      const meta = document.getElementById("js-meta");
+      const sdkSrc = document.querySelector("script[src*=skyway]");
+
+      // meta.innerText = `
+      //   UA: ${navigator.userAgent}
+      //   SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
+      // `.trim();
+
+      const localStream = await navigator.mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: true
+        })
+        .catch(console.error);
+
+      // Render local stream
+      localVideo.muted = true;
+      localVideo.srcObject = localStream;
+      localVideo.playsInline = true;
+      await localVideo.play().catch(console.error);
+
+      const peer = (window.peer = new Peer({
+        key: 'd8e43ecb-578b-414e-a161-2f00615b447e',
+        debug: 3
+      }));
+
+      // Register caller handler
+      callTrigger.addEventListener("click", () => {
+        // Note that you need to ensure the peer has connected to signaling server
+        // before using methods of peer instance.
+        if (!peer.open) {
+          return;
+        }
+
+        const mediaConnection = peer.call(remoteId.value, localStream);
+
+        mediaConnection.on("stream", async stream => {
+          // Render remote stream for caller
+          remoteVideo.srcObject = stream;
+          remoteVideo.playsInline = true;
+          await remoteVideo.play().catch(console.error);
+        });
+
+        mediaConnection.once("close", () => {
+          remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+          remoteVideo.srcObject = null;
+        });
+
+        closeTrigger.addEventListener("click", () =>
+          mediaConnection.close(true)
+        );
+      });
+
+      //Openすると、IDが払い出される。このIDで相手とのやり取りが開始される。
+      peer.once("open", id => (localId.textContent = id));
+
+      peer.on("call", mediaConnection => {
+        mediaConnection.answer(localStream);
+
+        mediaConnection.on("stream", async stream => {
+          // Render remote stream for callee
+          remoteVideo.srcObject = stream;
+          remoteVideo.playsInline = true;
+          await remoteVideo.play().catch(console.error);
+        });
+
+        mediaConnection.once("close", () => {
+          remoteVideo.srcObject.getTracks().forEach(track => track.stop());
+          remoteVideo.srcObject = null;
+        });
+
+        closeTrigger.addEventListener("click", () =>
+          mediaConnection.close(true)
+        );
+      });
+
+      //Errorになったらもとのページに戻す？
+      peer.on("error", console.error);
+    })();
+  }
+};
 </script>
