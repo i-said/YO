9ab12d50078f2aa58ca9a46406a18cb3ef3defc1
ikasumiwt <kato@atama.plus>
2020-05-31 17:04:39 +0900

    add dialog

---
+++ components/CallingProposalDialog.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <!-- Any other Bulma elements you want -->
+      <button style="
+        border-radius: 50%;
+        width: 200px;
+        height: 200px;
+        background-color: #248;
+        color: #fff;
+        border-style: #ffffff;
+        font-size:30pt;
+        font-weight:bold;
+        vertical-align:middle;
+        "
+        @click="acceptCalling">
+        <div class="card-content">
+          <div class="content yo-button has-text-centered">
+          </div>
+          通話したそうな人がいます。<br>
+          通話しますか？
+        </div>
+      </button>
+
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="removeThisUserFromWaitingList"></button>
+    </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const host = "yo-socketio.herokuapp.com"
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+  props: {
+  },
+  methods: {
+    ...mapMutations([
+      'peer/add'
+    ]),
+    acceptCalling() {
+      // TODO なにかルームが作られる
+      console.log("acceptCallingしました")
+      const room_id = this.$parent.waitingUsers[0];
+      this.$parent.socket.close();
+      this.$router.push({ path: '/calling', query: {'room_id': room_id , 'isOwner': false}})
+
+    },
+    removeThisUserFromWaitingList() {
+      const _waitingUsers = this.$parent.waitingUsers;
+      if (!this.$parent.waitingUsers.length) return;
+
+      this.$parent.waitingUsers = new Array(); //TODO fixme: remove one person from list
+    }
+  }
+}
+</script>
---
+++ components/CallingWaitDialog.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="removeThisUserFromWaitingList"></button>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
